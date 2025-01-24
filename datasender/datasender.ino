c6c658258d03fdd1dbbb851630fba9bf57194842
NeoDoggy <neodog0616@gmail.com>
2025-01-24 23:52:29 +0800

    update struct for espnow

--- datasender/datasender.ino
+++ datasender/datasender.ino
@@ -3,18 +3,22 @@
 #include <WiFi.h>
 #include "TinyGPS++.h"
 
+#define UTC 8
+
 uint8_t broadcastAddress[] = {0xC0, 0x4E, 0x30, 0x12, 0xC8, 0xA4};//c0:4e:30:12:c8:a4
 
 typedef struct struct_message {
   int speed;
+  char nowtime[15];                       // 202501081541006 => 15
 } struct_message;
 
-struct_message myData;
+struct_message enData;
 int speed;
 
 esp_now_peer_info_t peerInfo;
 TinyGPSPlus gps;
 
+String nowtime="202501010000003"; // 202501081541006 => 15
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
@@ -45,9 +49,31 @@ void loop() {
   }
   if(gps.speed.isUpdated()){
     speed=gps.speed.kmph();
-    myData.speed=speed;
-    Serial.print("\nSpeed: ");Serial.println(myData.speed);
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    enData.speed=speed;
+    Serial.print("\nSpeed: ");Serial.println(enData.speed);
+
+    nowtime="";
+    nowtime+=String(gps.date.year());
+    if(gps.date.month()<10)
+      nowtime+="0";
+    nowtime+=String(gps.date.month());
+    if(gps.date.day()<10)
+      nowtime+="0";
+    nowtime+=String(gps.date.day());
+    if((gps.time.hour()+UTC)%24<10)
+      nowtime+="0";
+    nowtime+=String((gps.time.hour()+UTC)%24);
+    if(gps.time.minute()<10)
+      nowtime+="0";
+    nowtime+=String(gps.time.minute());
+    if(gps.time.second()<10)
+      nowtime+="0";
+    nowtime+=String(gps.time.second());
+    nowtime+=String(0);
+    nowtime.toCharArray(enData.nowtime, nowtime.length()+1);
+    Serial.println("nowtime: "+nowtime);
+
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &enData, sizeof(enData));
     if (result == ESP_OK) {
       Serial.println("Sent with success");
     }
